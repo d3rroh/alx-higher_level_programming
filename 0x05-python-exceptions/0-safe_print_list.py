e108aced22418f4145874f629e5a272d5b5b8bd3
d3rroh <micaslounge@gmail.com>
2022-06-20 18:02:47 +0300

    df

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda val: val * number, my_list))
+def safe_print_list(my_list=[], x=0):
+    global count
+    try:
+        count = 0
+        for i in range(0, x, 1):
+            print(my_list[i], end="")
+            count += 1
+        print("")
+    except IndexError:
+        print("")
+    finally:
+        return count
