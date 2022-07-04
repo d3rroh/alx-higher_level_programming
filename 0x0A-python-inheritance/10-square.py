4e1e5928297e0e4dc1ad012a3bee6f015f7a02c8
d3rroh <micaslounge@gmail.com>
2022-07-04 22:44:24 +0300

    fg

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-'''task 9 module'''
+
+"""
+class Square - implements a square from a rectangle
+"""
 
 
-Rectangle = __import__('9-rectangle').Rectangle
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
-    '''classy class'''
-    def __init__(self, size):
-        '''initialization of squar'''
-        super().integer_validator('size', size)
-        self.__size = size
+    """implements a square from a rectangle"""
 
-    def area(self):
-        '''get that number'''
-        return self.__size ** 2
+    def __init__(self, size):
+        """constructor"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
