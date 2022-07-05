92d98fc87cf0a7f89948e039f692c649c4346e82
d3rroh <micaslounge@gmail.com>
2022-07-05 08:19:45 +0300

    gh

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,19 +1,21 @@
+
+
 #!/usr/bin/python3
-
 """
-class Square - implements a square from a rectangle
+Contains the class BaseGeometry and subclass Rectangle
 """
 
-
-Rectangle = __import__("9-rectangle").Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """implements a square from a rectangle"""
-
+    """A representation of a square"""
     def __init__(self, size):
-        """constructor"""
+        """instantiation of the square"""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
+    def area(self):
+        """"returns the area of the square"""
+        return self.__size ** 2
